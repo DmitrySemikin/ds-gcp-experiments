60ece21c011e652ffe560d26413970e6407a4e4f
Dmitrii Semikin <dmitry.semikin@gmail.com>
2023-07-06 16:05:14 +0200

    Provide code to create Google Cloud Run ServicesClient to work with services.

--- 02_change_access_cloud_run/app/src/main/java/xyz/dsemikin/tmp/gcp/run_access/App.java
+++ 02_change_access_cloud_run/app/src/main/java/xyz/dsemikin/tmp/gcp/run_access/App.java
@@ -1,11 +1,24 @@
 package xyz.dsemikin.tmp.gcp.run_access;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.google.cloud.run.v2.Service;
+import com.google.cloud.run.v2.ServiceName;
+import com.google.cloud.run.v2.ServicesClient;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+
+        // For it to work we need to provide credentials.
+        // See here: https://cloud.google.com/docs/authentication/provide-credentials-adc#how-to
+        try (final ServicesClient servicesClient = ServicesClient.create()) {
+            final ServiceName name = ServiceName.of("dsemikin-run-java-example-001", "us-central1", "ds-spring-hello");
+            final Service service = servicesClient.getService(name);
+            System.out.println(service);
+        }
+
+        System.out.println("Good Bye");
     }
 }
