e8a338d51fa76ca75fe20ae61bffba3df202cefd
Dmitrii Semikin <D.Semikin@SAB-Engineering.com>
2023-07-28 17:07:16 +0200

    Add and use hero.service

--- 04_angular_tour_of_heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ 04_angular_tour_of_heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,8 +9,22 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent {
 
-  heroes = HEROES;
+  constructor(
+    private heroService: HeroService
+  ) {
+    // empty
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
